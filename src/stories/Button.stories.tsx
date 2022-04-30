23c3f77d1cdf5b8812e43d67d2bd83fa5ac3d455
Everton <thi.ger@hotmail.com>
2022-04-30 18:58:22 -0300

    Finalizando o compnente Botão com o Polished

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,38 +1,48 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button, { ButtonProps } from '../Components/Button/Button';
+import Button from '../Components/Button/Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  // // argTypes: {
+  // //   backgroundColor: { control: 'color' },
+  // },
 } as ComponentMeta<typeof Button>;
+
+///////////////////////////////////////////
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+///////////////////////////////////////////
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   variant: 'primary',
-  label: 'Button',
-};
-
-export const Danger = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Danger.args = {
-  variant: 'danger',
-  label: 'Button',
+  label: 'Resetar senha',
 };
 
 export const Text = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Text.args = {
   variant: 'text',
-  label: 'Button',
+  label: 'Resetar senha',
 };
 
+export const Danger = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Danger.args = {
+  variant: 'danger',
+  label: 'Resetar senha',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  variant: 'primary',
+  disabled: true,
+  label: 'Resetar senha',
+};
